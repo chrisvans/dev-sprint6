d2172d8f60c8ad0925685b6026d2f2bbe5ccde66
chrisvans <chrisvanschyndel@gmail.com>
2013-03-16 23:08:13 -0400

    Exercises Complete

--- chap14.py
+++ chap14.py
@@ -1,1 +1,29 @@
 # This is where chapter 14 exercises go.
+
+# Exercise 14.6
+
+import urllib
+
+print 'Please enter in a zip code.'
+print 'The corresponding location and population will be given.'
+zip_code = raw_input('> ')
+conn = urllib.urlopen('http://www.uszip.com/zip/'+zip_code)
+for line in conn:
+	if line.strip()[:7] == '<title>':
+		name = line.strip()[7:]
+		name = name[:-17]
+		if name == ', ':
+			print 'No location found'
+		else:
+			print name 
+	if 'Total population' in line.strip():
+		indexor = line.strip()
+		indexor2 = indexor
+		indexor = indexor.find('Total population')
+		indexor2 = indexor2[indexor+25:]
+		indexor2 = indexor2.find('<')
+		population = line.strip()[indexor+25:indexor+25+indexor2]
+		print population
+
+
+
--- chap15.py
+++ chap15.py
@@ -1,1 +1,47 @@
 # This is where chapter 15 exercises go.
+
+# Exercise 15.3
+
+class Point(object):
+	"""Represents a point in 2-D space."""
+
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def position(self):
+		return self.x, self.y
+
+class Rectangle(object):
+	"""Represents a rectangle.
+
+	attributes: width, height, corner
+	"""
+
+	def __init__(self, width, height, corner_x, corner_y):
+		self.width = width
+		self.height = height
+		self.corner = Point(corner_x, corner_y)
+
+	def find_center(self):
+		p = Point(self.corner.x + self.width/2.0, self.corner.y + self.height/2.0)
+		return p.position()
+
+	def grow_rectangle(self, width, height):
+		self.width += width
+		self.height += height
+
+	def move_rectangle(self, x, y):
+		self.corner.x += x
+		self.corner.y += y
+
+def move_rectangle(rect, dx, dy):
+	# Creates a new rectangle at the new point, instead of moving the old one.
+	# The function name is strange for this operation.
+	new_rect = Rectangle(rect.width, rect.height, rect.corner.x, rect.corner.y)
+	new_rect.move_rectangle(dx, dy)
+	return new_rect
+
+rectyboy = Rectangle(50, 100, 2, 5)
+new_location = move_rectangle(rectyboy, 25, 25)
+print new_location.width, new_location.height, new_location.corner.x, new_location.corner.y
